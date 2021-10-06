32fc310b0c377650bc920a3d156c07775a5a7748
HaarikaR <hramadugu@gmail.com>
2021-10-06 11:05:40 -0700

    Priting wrong answers

--- index.js
+++ index.js
@@ -1,16 +1,42 @@
 //lets play roulette event listerner
 const letsplayBtn = document.getElementById('letsplay')
+const answerBtn = document.getElementById("answer-buttons")
 letsplayBtn.addEventListener("click",() => {document.getElementById("disappear").innerHTML ="";
 const BASE_URL = "https://opentdb.com/api.php?amount=5&type=boolean"
 fetch(BASE_URL)
 .then(response => response.json()
 .then((data =>{
-        
-data.results.map(element => {
+    const newArr = data.results       
+newArr.map(element => {
             const displayQuestion = {
                 question : element.question,
             };
+            
             console.log(displayQuestion);
+           
+          console.log(data.results)
+          if(element.correct_answer === answerBtn){
+            return true;
+        }else{
+            console.log( element.incorrect_answers)
+            
+        }
+          
+            // const answers = 0;
+            // let choice = '';
+            // for( choice in displayQuestion.correct_answer){
+                
+            // }
+            
+           
         })
+            
+           // console.log(element.correct_answer)
+           
+        
+
+        
 })
 ))})
+
+
