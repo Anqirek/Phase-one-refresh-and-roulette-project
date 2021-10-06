b7b9bda573c46120a412e30f5231adf0632d96bf
HaarikaR <hramadugu@gmail.com>
2021-10-05 21:07:34 -0700

    Fetched single question

--- index.js
+++ index.js
@@ -3,19 +3,14 @@ const letsplayBtn = document.getElementById('letsplay')
 letsplayBtn.addEventListener("click",() => {document.getElementById("disappear").innerHTML ="";
 const BASE_URL = "https://opentdb.com/api.php?amount=5&type=boolean"
 fetch(BASE_URL)
-.then(response => response.json())
+.then(response => response.json()
 .then((data =>{
-           //onst response1 = Object.values(question.results)
-        data.results.forEach(element => {
-            console.log(element.question);
-        });
-         
-   
-       //response1.forEach(results => results.question));
-        //console.log(results.question)
-    }
-   
-
-
-   // console.log(letsplayBtn)
+        
+data.results.map(element => {
+            const displayQuestion = {
+                question : element.question,
+            };
+            console.log(displayQuestion);
+        })
+})
 ))})
