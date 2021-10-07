4177b898914a261c244fc4e5dfe42877caf30f73
HaarikaR <hramadugu@gmail.com>
2021-10-06 19:09:02 -0700

    Answers rendered

--- index.js
+++ index.js
@@ -1,70 +1,59 @@
-var i =0;
+
 const letsplayBtn = document.getElementById('letsplay')
-const answerBtn = document.getElementById("answer-buttons")
-//console.log(answerBtn)
-const slides = document.querySelectorAll(".slide")
 const nextButton = document.getElementById("next");
+const trueBtn = document.getElementById("True")
+const falseBtn = document.getElementById('False')
 const BASE_URL = "https://opentdb.com/api.php?amount=5&type=boolean"
 let currentSlide = 0;
-const answers = []; // to store the answers
+//const answers = []; // to store the answers
+let total_correct = 0;   // keep track of user's answers
+
 let newArr;
+let i = 0;
+const output = [];
 
-// function showSlide(){
-//     slides[currentSlide].classList.remove('active-slide');
-//     slides[n].classList.add('active-slide');
-//      currentSlide = n;
-//      if(currentSlide === 0){
-//          nextButton.style.display = 'inline-block'
-//      }else{
-
-//      }
-//      if(currentSlide === slides.length-1){
-//          nextButton.style.display = 'none';
-
-//      }
-// }
-
-//event listeners
-nextButton.addEventListener("click",()=>{
+//event listener for next button
+nextButton.addEventListener("click", () => {
     let displayQuestion;
-    console.log(newArr[i].question)
-    displayQuestion = newArr[i];
-    console.log(i+'\n'+newArr.length)
-    if(i>=newArr.length-1){
-        document.getElementById('next').innerText = 'Submit'
+    //getting the answer selected by the user and reading it into selected_answer
+    let selected_answer = document.querySelector('input[name="Answer"]:checked').value;
+    console.log(selected_answer)
+    console.log(newArr[i].correct_answer)
+    //comparing selected answer with the correct asnwer if correct incrementing the right count
+    if (selected_answer === newArr[i].correct_answer) {
+        total_correct++;
     }
-    document.getElementById('question').innerText = displayQuestion.question
-    i++;
-
+    //checking if this is  the last question
+    if (i < newArr.length - 1) {
+        //moving on to next question rendering
+        i++;
+        displayQuestion = newArr[i];
+        document.getElementById('question').innerText = displayQuestion.question
+        console.log(displayQuestion.question)
+        //if this is the last question changing next quetion to submit
+        if (i === newArr.length - 1) {
+            document.getElementById('next').innerText = 'Submit' //replacing next question with submit as the text
+        }
+    }
+    else {
+        document.getElementById('question').innerText = `you have total ${total_correct} correct answers`
+    }
 })
 
 //lets play roulette event listerner
-letsplayBtn.addEventListener("click",() =>
-{document.getElementById("disappear").innerHTML ="";
-fetch(BASE_URL)
-.then(response => response.json()
-.then((data =>{
- newArr = data.results       
- console.log(newArr[i].question)
-const displayQuestion = newArr[i];
-i++;
-// newArr.map(element => {        //for each question add it with current answer
-//                                // output to add question to its answer and render it with true or false button
-//     const displayQuestion = {
-//         question : element.question,
-//     };
-     document.getElementById('question').innerText = displayQuestion.question
-//     //console.log(displayQuestion);
-    
-//     console.log(data.results)
-//     if(element.correct_answer === answerBtn){
-//     return true;
-//     }else{
-//     console.log( element.incorrect_answers)
-    
-// }
-// })
+letsplayBtn.addEventListener("click", () => {
+    document.getElementById("disappear").innerHTML = "";
+    fetch(BASE_URL)
+        .then(response => response.json()
+            .then((data => {
+                newArr = data.results
+                console.log(newArr)
+                const displayQuestion = newArr[i];
+                document.getElementById('question').style = 'display:block'
+                document.getElementById('answer-buttons').style = 'display:block'
+                document.getElementById('next').style = 'display:block'
+                document.getElementById('question').innerText = displayQuestion.question  //displaying first question from the array
+                console.log(displayQuestion.question)
+            })
+            ))
 })
-))})
-
-
